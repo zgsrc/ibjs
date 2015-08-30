4de26dea3a7077e9caf903ab4cfa3d7b7c613e2d
Jonathan Hollinger <jonathanhollinger@gmail.com>
2015-08-29 20:02:31 -0600

    Update tests

--- test/connection.js
+++ test/connection.js
@@ -4,21 +4,125 @@ var expect = chai.expect;
 chai.should();
 
 describe('IB Connection', function() {
-    var cxn = require('../lib/connection');
+    var ib = require('../lib/connection'),
+        constants = require("../lib/constants"),
+        connection = null;
     
-    it('Connects to a running IB API instance', function(done) {    
-        var connection = cxn.connect({ }, function(err, status) {
-            expect(err).to.be.a("null");
+    ib.clientSeed = 100;
+    it('can connect to IB API interface', function(done) {    
+        connection = new ib.Connection();
+        connection.connect(function(err, status) {
+            expect(err).to.be.null;
             status.should.equal("connected");
             done();
+        });
+    });
+    
+    it('can fetch the current server time', function(done) {
+        connection.currentTime(function(err, time) {
+            expect(err).to.be.null;
+            time.should.be.a("number");
+            done();
+        });
+    });
+    
+    it('can disconnect', function(done) {
+        connection.disconnect(function(err, status) {
+            expect(err).to.be.null;
+            status.should.equal("disconnected");
+            done();
+        });
+    });
+    
+    it('cannot disconnect twice', function(done) {
+        connection.disconnect(function(err, status) {
+            expect(err).to.be.ok;
+            status.should.equal("error");
+            done();
+        });
+    });
+    
+    it('can reconnect', function(done) {
+        connection.connect(function(err, status) {
+            expect(err).to.be.null;
+            status.should.equal("connected");
+            done();
+        });
+    });
+    
+    it('can connect twice', function(done) {
+        connection.connect(function(err, status) {
+            expect(err).to.be.null;
+            status.should.equal("connected");
+            done();
+        });
+    });
+    
+    var contract = null;
+    it('can create AAPL contract', function() {
+        contract = connection.contract.stock("AAPL");
+        contract.should.be.ok;
+        contract.symbol.should.equal("AAPL");
+    });
+    
+    it('can get APPL contract details', function(done) {
+        connection.security(contract, function(err, details) {
+            expect(err).to.be.null;
+            details.should.be.ok;
+            done();
+        });
+    });
+    
+    it('can get AAPL fundamentals', function(done) {
+        connection.fundamentals(contract, constants.REPORT.snapshot, function(err, report, cancel) {
+            expect(err).to.be.null;
+            report.should.be.an("object");
+            cancel.should.be.a("function");
             
-            it('Can fetch the current server time', function(done) {
-                connection.currentTime(function(err, time) {
-                    expect(err).to.be.a("null");
-                    time.should.be.a("number");
-                    done();
-                });
-            });
+            cancel();
+            done();
+        });
+    });
+    
+    it('can get AAPL historical pricing', function(done) {
+        this.timeout(10000);
+        connection.historicals(contract, { }, function(err, bar, cancel) {
+            expect(err).to.be.null;
+            bar.should.be.an("object");
+            cancel.should.be.a("function");
+            
+            if (bar.date.startsWith("finished")) {
+                cancel();
+                done();
+            }
+        });
+    });
+    
+    it('can get a quote for AAPL', function(done) {
+        connection.quote(contract, null, function(err, quote, cancel) {
+            expect(err).to.be.null;
+            quote.should.be.an("object");
+            cancel.should.be.a("function");
+            
+            cancel();
+            done();
+        });
+    });
+    
+    it('can get a snapshot quote for AAPL', function(done) {
+        connection.snapshot(contract, function(err, quote, cancel) {
+            expect(err).to.be.null;
+            quote.should.be.an("object");
+            cancel.should.be.a("function");
+            
+            cancel();
+            done();
+        });
+    });
+    
+    after(function(done) {
+        connection.disconnect(function() {
+            done();
         });
     });
 });
