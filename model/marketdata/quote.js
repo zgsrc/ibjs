b7e76f8ffa7eb8190ae9e23a438f337f3ae058d2
Jonathan Sterling Hollinger <src@jonholl.com>
2017-07-11 13:12:42 -0400

    Fix buffers

--- model/marketdata/quote.js
+++ model/marketdata/quote.js
@@ -2,6 +2,8 @@
 
 require("sugar").extend();
 
+Date.getLocale('en').addFormat('{yyyy}{MM}{dd}-{hh}:{mm}:{ss}');
+
 const MarketData = require("./marketdata"),
       flags = require("../flags"),
       TICKS = flags.QUOTE_TICK_TYPES;
@@ -105,6 +107,10 @@ class Quote extends MarketData {
         return new RealTimeVolumeBuffer(this, duration || 5000);
     }
     
+    newsBuffer(duration) {
+        return new NewsBuffer(this, duration || 60000 * 60);
+    }
+    
 }
 
 function parseQuotePart(datum) {
@@ -112,8 +118,11 @@ function parseQuotePart(datum) {
     
     if (!key || key == "") throw new Error("Tick key not found.");
     if (value === null || value === "") throw new Error("No tick data value found.");
-    if (key == "LAST_TIMESTAMP") value = new Date(parseInt(value) * 1000);
-    if (key == "RT_VOLUME") {
+    
+    if (key == "LAST_TIMESTAMP") {
+        value = new Date(parseInt(value) * 1000);
+    }
+    else if (key == "RT_VOLUME") {
         value = value.split(";");
         value = {
             price: parseFloat(value[0]),
@@ -122,6 +131,26 @@ function parseQuotePart(datum) {
             volume: parseInt(value[3]),
             vwap: parseFloat(value[4]),
             marketMaker: new Boolean(value[5])
+        };
+    }
+    else if (key == "FUNDAMENTAL_RATIOS") {
+        let ratios = { };
+        value.split(";").forEach(r => {
+            let parts = r.split("=");
+            if (parts[0].trim().length > 0) {
+                ratios[parts[0]] = parseFloat(parts[1]);
+            }
+        });
+        
+        value = ratios;
+    }
+    else if (key == "NEWS_TICK") {
+        value = value.split(" ");
+        value = {
+            id: value[0],
+            time: value[1],
+            source: value[2],
+            text: value.from(3).join(' ')
         };
     }
     
@@ -133,7 +162,12 @@ class RealTimeVolumeBuffer extends MarketData {
     constructor(quote, duration) {
         super(quote.session, quote.contract);
         
+        this.duration = duration;
         this.history = [ ];
+        
+        if (quote.rtVolume) {
+            this.history.push(quote.rtVolume);
+        }
         
         quote.on("update", data => {
             if (data.key == "rtVolume") {
@@ -147,7 +181,38 @@ class RealTimeVolumeBuffer extends MarketData {
     
     prune() {
         let now = (new Date()).getTime();
-        while (this.history.length && now - this.history.first().time.getTime() > duration) {
+        while (this.history.length && now - this.history.first().time.getTime() > this.duration) {
+            this.history.shift();
+        }
+    }
+    
+}
+
+class NewsBuffer extends MarketData {
+    
+    constructor(quote, duration) {
+        super(quote.session, quote.contract);
+        
+        this.duration = duration;
+        this.history = [ ];
+        
+        if (quote.newsTick) {
+            this.history.push(quote.newsTick);
+        }
+        
+        quote.on("update", data => {
+            if (data.key == "newsTick") {
+                this.history.push(data.newValue);
+                this.prune();
+                setInterval(() => this.prune(), duration);
+                this.emit("update", data);
+            }
+        });
+    }
+    
+    prune() {
+        let now = (new Date()).getTime();
+        while (this.history.length && now - this.history.first().time.getTime() > this.duration) {
             this.history.shift();
         }
     }
