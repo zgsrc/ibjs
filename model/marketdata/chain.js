405b9449dc265198aa4c0860ead822d997d93c60
Jonathan Sterling Hollinger <src@jonholl.com>
2017-08-12 12:46:55 -0400

    Add option chains and expiry curves.

--- model/marketdata/chain.js
+++ model/marketdata/chain.js
@@ -4,7 +4,7 @@ const MarketData = require("./marketdata");
 
 class Chain extends MarketData {
     
-    constructor(session, securities) {
+    constructor(session, securities, symbol) {
         super(session, securities.sortBy(s => s.contract.expiry).map("contract"));
         
         let expirations = securities.groupBy(s => s.contract.summary.expiry);
@@ -14,9 +14,10 @@ class Chain extends MarketData {
                 calls: expirations[date].filter(s => s.contract.summary.right == "C").sortBy("strike"),
                 puts: expirations[date].filter(s => s.contract.summary.right == "P").sortBy("strike")
             };
-        })
+        });
         
         Object.defineProperty(this, "expirations", { value: expirations });
+        Object.defineProperty(this, "symbol", { value: symbol || this.contract.first().summary.symbol + "_options" });
     }
     
 }
--- model/marketdata/curve.js
+++ model/marketdata/curve.js
@@ -4,9 +4,10 @@ const MarketData = require("./marketdata");
 
 class Curve extends MarketData {
     
-    constructor(session, securities) {
+    constructor(session, securities, symbol) {
         super(session, securities.sortBy(s => s.contract.expiry).map("contract"));
         Object.defineProperty(this, "securities", { value: securities.sortBy(s => s.contract.expiry) });
+        Object.defineProperty(this, "symbol", { value: symbol || this.contract.first().summary.symbol + "_options" });
     }
     
     stream() {
