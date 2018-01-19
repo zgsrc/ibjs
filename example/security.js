3e69e9f97d4522da3af332a710c23f6d74fd0d17
Jonathan Sterling Hollinger <src@jonholl.com>
2018-01-19 08:04:20 -0500

    Update docs

--- example/security.js
+++ example/security.js
@@ -4,26 +4,13 @@ const sdk = require("..");
 
 sdk.start().then(async session => {
     
-    let CL = (await session.securities("Front CL future on NYMEX"))[0];
-    
-console.log(CL.contract.marketsOpen);
-    console.log(CL.contract.marketsLiquid);
-    
-    let AAPL = (await session.securities(172603798))[0];
-    console.log(AAPL.contract);
-    
-    
-    /*
     let AAPL = (await session.securities("AAPL stock"))[0];
     console.log(AAPL.contract);
-    console.log(CL.contract);
-    
     
     let snapshot = await AAPL.fundamentals("snapshot");
     console.log("SNAPSHOT");
     console.log(snapshot);
-    */
     
-
     session.close();
+    
 }).catch(console.log);
