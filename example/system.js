7aa3916b7fa8f65def7722270be56d289206db77
Jonathan Sterling Hollinger <src@jonholl.com>
2018-01-20 10:44:45 -0500

    Update docs

--- example/system.js
+++ example/system.js
@@ -6,7 +6,10 @@ sdk.start().then(async session => {
     
     session.on("error", console.log);
     session.on("disconnected", () => console.log("Disconnected."));
+    session.on("connectivity", console.log);
+    session.on("displayGroupUpdated", group => console.log(group.contract));
     
+    // Make sure stuff has loaded
     await session.system();
     
     // IB news bulletins (margin calls, special labelling, etc)
@@ -19,13 +22,9 @@ sdk.start().then(async session => {
     let connectivity = session.connectivity;
     console.log(connectivity);
     
-    session.on("connectivity", console.log);
-
     // Access display groups
     session.displayGroups.forEach(group => console.log(group.contract));
     
-    session.on("displayGroupUpdated", group => console.log(group.contract));
-
     // Update display group
     session.displayGroups[0].update("8314");
 
