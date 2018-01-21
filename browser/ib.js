ea2da4a9cb72da0fe08a0208a60e6d97d72159a7
Jonathan Sterling Hollinger <src@jonholl.com>
2018-01-21 10:40:28 -0500

    Update browser

--- browser/ib.js
+++ browser/ib.js
@@ -2085,7 +2085,26 @@ function securities(session, description, cb) {
 
 module.exports = securities;
 },{"../flags":8,"./charts":11,"./contract":12,"./depth":14,"./marketdata":15,"./order":16,"./quote":17}],19:[function(require,module,exports){
-module.exports = { };
+module.exports = { 
+
+    MIN: window => window.min("low"),
+    MAX: window => window.max("high"),
+    MOM: window => window.last().close - window.first().open,
+    
+    SMA: window => window.average("close"),
+    WMA: window => window.length.downto(1).map((n,i) => n * window[i]) / (window.length * (window.length + 1) / 2),
+    EMA: (window, name) => {
+        let P = window.at(-1).close,
+            EMAp = window.at(-2)[name],
+            n = window.length,
+            K = 2 / (n + 1);
+
+        return ( P - EMAp ) * K + EMAp;
+    },
+    
+    VWAP: window => window.map(bar => bar.volume * [ bar.high, bar.low, bar.close ].average()).sum() / window.sum("volume")
+
+};
 },{}],20:[function(require,module,exports){
 "use strict";
 
