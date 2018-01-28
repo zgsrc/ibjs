b85204d421f8bcc5dc7ad65d2d069a0182a16d20
Jonathan Sterling Hollinger <src@jonholl.com>
2018-01-27 19:10:12 -0500

    Update studies

--- model/marketdata/studies.js
+++ model/marketdata/studies.js
@@ -15,16 +15,11 @@ module.exports = {
         return ( P - EMAp ) * K + EMAp;
     },
     
-    VWAP: window => window.map(bar => bar.volume * [ bar.high, bar.low, bar.close ].average()).sum() / window.sum("volume"),
-
-    
     ABANDS: window => ({
         upper: window.map(b => b.high * (1 + 4 * (b.high - b.low) / (b.high + b.low))).average(),
         middle: window.average("close"),
         lower: window.map(b => b.low * (1 - 4 * (b.high - b.low) / (b.high + b.low))).aveage()
     }),
-    AD: (window, name) => {
-        return (window.at(-2)[name] || 0) + (((window.last().close - window.last().low) - (window.last().high - window.last().close)) / (window.last().high - window.last().low)) * window.last().volume
-    }
+    VWAP: window => window.map(bar => bar.volume * [ bar.high, bar.low, bar.close ].average()).sum() / window.sum("volume")
     
 };
